4ce496b4ed112587489f8d2ed3fc4b35104992e4
robinhood <robin.schellemans174@gmail.com>
2020-09-01 17:19:03 +0200

    i dunno

--- bee.py
+++ bee.py
@@ -6,7 +6,8 @@ import mysql.connector
 from discord.ext import commands
 
 mydb = mysql.connector.connect(
-    host="localhost",
+    host="192.168.0.45",
+    port='4444',
     user="robin",
     password="password",
     database="mydb"
@@ -20,11 +21,25 @@ print("starting up bee ...")
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(activity=discord.Game(name='bzzzzzzzzzzzzzzzzz'))
     print("bee is ready!")
 
 @bot.command()
 async def hi(ctx):
-    await ctx.send("hyellow")
+    if ctx.message.author.name == "heyimlu":
+        await ctx.send("hey lu you get two hugs because you're awesome, <:ghosthugright:748209219714547764> <:ghosthugright:748209219714547764>")
+        return
+    if ctx.message.author.name == "Charli":
+        await ctx.send("hi charli bee, mama bee believes you can do anything <:ghosthugright:748209219714547764>")
+        return
+    if ctx.message.author.name == "THE SEER":
+        await ctx.send("hi seer, bee just wants to say she loves you.")
+        return
+    if ctx.message.author.name == "KermisKlant":
+        await ctx.send("")
+    if ctx.message.author.name == "Cavemaker":
+        await ctx.send("hi plop, have a plop cookie 🍪")
+    await ctx.send("hyellow <:ghosthugright:748209219714547764>")
 
 @bot.command()
 async def blackjack(ctx):
@@ -35,8 +50,19 @@ async def blackjack(ctx):
 @bot.command()
 async def stickbug(ctx):
     await ctx.send(file=discord.File('stickbug.gif'))
-
-
+@bot.command()
+async def cookie(ctx):
+    await ctx.send("🍪")
+@bot.command()
+async def cookies(ctx, *arg):
+    cookies = 3
+    test = 1
+    while test <= cookies:
+        await ctx.send("🍪")
+        test = test + 1
+    if arg[0] == "infinite":
+        while True:
+            await ctx.send("🍪")
 
 
 
--- blackjack.py
+++ blackjack.py
@@ -33,10 +33,14 @@ bot.cards = [("ace heart", 1), ("ace clubs", 1), ("ace diamonds", 1), ("ace spad
 
 
 
+bot.busted = []
+bot.standing = []
+bot.cardnumber = 0
+bot.pot = 0
 
-def hit():
+
+async def deal_card():
     print("test")
-
 
 bot.started = False
 @bot.event
@@ -76,33 +80,33 @@ async def start(ctx):
         return
     else:
         bot.started = True
-        cardnumber = 0
         score = 0
         test = 1
         while test <= 2:
 
             for player in bot.players:
                 user = bot.get_user(player[2])
-                if bot.cards[cardnumber][1] == 1:
+                if bot.cards[bot.cardnumber][1] == 1:
                     bot.ace.append(player)
                 cursor.execute("SELECT score FROM main WHERE username = '%s'"%player[0], multi=False)
                 currentscore = cursor.fetchall()
-                score = currentscore[0][0] + bot.cards[cardnumber][1]
+                score = currentscore[0][0] + bot.cards[bot.cardnumber][1]
                 cursor.execute("UPDATE main SET score = %s WHERE username = '%s'"%(score, player[0]), multi = False)
                 mydb.commit()
                 cursor.execute("SELECT score FROM main WHERE username = '%s'"%(player[0]), multi = False)
                 result = cursor.fetchall()
-                await user.send(bot.cards[cardnumber][0])
-                cardnumber = cardnumber + 1
+                await user.send(bot.cards[bot.cardnumber][0])
+                bot.cardnumber = bot.cardnumber + 1
             test = test + 1
         for player in bot.players:
             user = bot.get_user(player[2])
             cursor.execute("SELECT score FROM main WHERE username = '%s'"%(player[0]), multi = False)
             result = cursor.fetchall()
-            if player in bot.ace:
+            if player in bot.ace and result[0][0] + 10 < 21:
                 await user.send("you currently have: %s or %s"%(str(result[0][0]), str(result[0][0]+ 10)))
             else:
                 await user.send("you currently have: " + str(result[0][0]))
+                
         
         random.shuffle(bot.players)
         playerOrder = []
@@ -110,15 +114,27 @@ async def start(ctx):
             playerOrder.append(player[0]) 
         playerOrder = '\n'.join(playerOrder)
         await ctx.send("it's time to place your bets in this order:\n" + playerOrder)
+    for player in bot.players:
+        cursor.execute("SELECT score FROM main WHERE username = '%s'"%player[0], multi=False)
+        result = cursor.fetchall()
+        if result[0][0] > 21:
+            bot.busted.append(player[0])
+            channel = bot.get_channel(748251645242114078)
+            await channel.send(player[0] + " is busted")
         
 bot.playernumber = 0
 bot.stillToBet = []
 @bot.command()
 async def hit(ctx, arg):
     name = ctx.message.author.name
+    if name in bot.standing:
+        await ctx.send("You can't get another card anymore, you're standing")
     
     bot.highestBet = 0
     if int(arg) > bot.highestBet: bot.highestBet = int(arg)
+    if int(arg) < bot.highestBet:
+        await ctx.send("you're not betting enough")
+        return
     try:
         if name == bot.players[bot.playernumber][0]:
             bot.playernumber = bot.playernumber + 1
@@ -128,6 +144,7 @@ async def hit(ctx, arg):
                 newCurrency = oldCurrency[0][0] - int(arg)
                 if newCurrency < 0:
                     await ctx.send("I'm sorry but you don't have enough chips to afford that, contact robinhood to buy more.")
+                    playernumber = playernumber - 1
                     return
                 else:
                     cursor.execute("SELECT bet FROM main WHERE username = '%s'"%name, multi=False)
@@ -137,6 +154,7 @@ async def hit(ctx, arg):
                     cursor.execute("UPDATE main SET currency = %s WHERE username = '%s'"%(newCurrency, name), multi=False)
                     cursor.execute("UPDATE main SET bet = %s WHERE username = '%s'"%(totalBet, name), multi=False)
                     mydb.commit()
+                    bot.pot = bot.pot + arg
             except IOError as error:
                 print(error)
                 await ctx.send("You have to provide the amount you want to bet like this ``b hit 420``.")
@@ -144,7 +162,6 @@ async def hit(ctx, arg):
             try:
                 await ctx.send(bot.players[bot.playernumber][0] + " you're up next to place your bet, stand or leave.")
             except:
-                bot.stillToBet = []
                 for player in bot.players:
                     cursor.execute("SELECT bet FROM main WHERE username = '%s'"%player[0], multi=False)
                     playerBet = cursor.fetchall()
@@ -153,12 +170,34 @@ async def hit(ctx, arg):
                     else:
                         continue
                 if bot.stillToBet:
-                    secondBet = True 
                     await ctx.send("these people still have to bet")
                     for x in bot.stillToBet:
                         await ctx.send("%s: %d"%(x[0],x[1]))
                 else:
                     await ctx.send("Ok bets are placed the next card will be dealt")
+                    bot.cardnumber = 0
+                    for player in bot.players:
+                        user = bot.get_user(player[2])
+                        if bot.cards[bot.cardnumber][1] == 1:
+                            bot.ace.append(player)
+                        cursor.execute("SELECT score FROM main WHERE username = '%s'"%player[0], multi=False)
+                        currentscore = cursor.fetchall()
+                        score = currentscore[0][0] + bot.cards[bot.cardnumber][1]
+                        cursor.execute("UPDATE main SET score = %s WHERE username = '%s'"%(score, player[0]), multi = False)
+                        mydb.commit()
+                        cursor.execute("SELECT score FROM main WHERE username = '%s'"%(player[0]), multi = False)
+                        result = cursor.fetchall()
+                        await user.send(bot.cards[bot.cardnumber][0])
+                        bot.cardnumber = bot.cardnumber + 1  
+                    for player in bot.players:
+                        user = bot.get_user(player[2])
+                        cursor.execute("SELECT score FROM main WHERE username = '%s'"%(player[0]), multi = False)
+                        result = cursor.fetchall()
+                        if player in bot.ace and result[0][0] + 10 < 21:
+                            await user.send("you currently have: %s or %s"%(str(result[0][0]), str(result[0][0]+ 10)))
+                        else:
+                            await user.send("you currently have: " + str(result[0][0]))
+            
         else:
             print("not your turn")
     except:
@@ -204,43 +243,73 @@ async def hit(ctx, arg):
                     else:
                         continue
             if bot.stillToBet:
-                secondBet = True 
                 await ctx.send("these people still have to bet")
                 for x in bot.stillToBet:
                     await ctx.send("%s: %d"%(x[0],x[1]))
             else:
                 await ctx.send("Ok bets are placed the next card will be dealt")  
 
-                cardnumber = 0
+
                 for player in bot.players:
                     user = bot.get_user(player[2])
-                    if bot.cards[cardnumber][1] == 1:
+                    if bot.cards[bot.cardnumber][1] == 1:
                         bot.ace.append(player)
                     cursor.execute("SELECT score FROM main WHERE username = '%s'"%player[0], multi=False)
                     currentscore = cursor.fetchall()
-                    score = currentscore[0][0] + bot.cards[cardnumber][1]
+                    score = currentscore[0][0] + bot.cards[bot.cardnumber][1]
                     cursor.execute("UPDATE main SET score = %s WHERE username = '%s'"%(score, player[0]), multi = False)
                     mydb.commit()
                     cursor.execute("SELECT score FROM main WHERE username = '%s'"%(player[0]), multi = False)
                     result = cursor.fetchall()
-                    await user.send(bot.cards[cardnumber][0])
-                    cardnumber = cardnumber + 1  
+                    await user.send(bot.cards[bot.cardnumber][0])
+                    bot.cardnumber = bot.cardnumber + 1  
                 for player in bot.players:
                     user = bot.get_user(player[2])
                     cursor.execute("SELECT score FROM main WHERE username = '%s'"%(player[0]), multi = False)
                     result = cursor.fetchall()
-                    if player in bot.ace:
+                    if player in bot.ace and str(result[0][0] + 10) < 21:
                         await user.send("you currently have: %s or %s"%(str(result[0][0]), str(result[0][0]+ 10)))
                     else:
                         await user.send("you currently have: " + str(result[0][0]))
-    
+    for player in bot.players:
+        cursor.execute("SELECT score FROM main WHERE username = '%s'"%player[0], multi=False)
+        result = cursor.fetchall()
+        if result[0][0] > 21:
+            bot.busted.append(player[0])
+            channel = bot.get_channel(748251645242114078)
+            await channel.send(player[0] + " is busted")
+            for player in bot.players:
+                if player in bot.standing or player in bot.busted:
+                    print("game ended these are the scores")
                 
             #print("these people still have to bet: ")
-                    
+bot.end = False                    
+@bot.command()
+async def stand(ctx):
+    name = ctx.message.author.name
+    bot.standing.append(name)
     
-                
-
-
+    if name in bot.ace:
+        cursor.execute("SELECT score FROM main WHERE username = '%s'"%name, multi=False)
+        result = cursor.fetchall()
+        if result[0][0] + 10 <= 21:
+            score = result[0][0] + 10
+            cursor.execute("UPATE main SET score WHERE username = '%s'"%name)
+    for player in bot.players:
+        print("test")
+        print(player[0], bot.standing, bot.busted, bot.end)
+        print(bot.standing)
+        if player[0] in bot.standing or player[0] in bot.busted:
+            print("test1")
+            bot.end = True
+        else: 
+            bot.end = False
+            break
+    if bot.end:
+        print("game ended these are the scores")
+        cursor.execute("SELECT * FROM main WHERE score > 0 ORDER BY score ASC")
+        result = cursor.fetchall()
+        print(result)
 
 
     
